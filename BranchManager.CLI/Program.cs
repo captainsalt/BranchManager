64465099d482a25582d9bd4c68b71d178f8a11ce
captainsalt <dontbothertheking@gmail.com>
2026-01-07 17:52:34 -0600

    Rename functions

--- BranchManager.CLI/Program.cs
+++ BranchManager.CLI/Program.cs
@@ -7,15 +7,15 @@ internal class Program
     private static void Main(string[] args)
     {
         var repositories = Core.BranchManager.GetBranches(new DirectoryInfo(""));
-        var multiSelection = GenerateMultiSelection(repositories);
-        var selected = AnsiConsole.Prompt(multiSelection);
+        var prompt = GeneratePrompt(repositories);
+        var selected = AnsiConsole.Prompt(prompt);
 
         Core.BranchManager.DeleteBranches(selected);
     }
 
-    static MultiSelectionPrompt<Branch> GenerateMultiSelection(List<Branch> branches)
+    static MultiSelectionPrompt<Branch> GeneratePrompt(List<Branch> branches)
     {
-        var padding = branches.Select(b => b.DirInfo.Name.Length).Max();
+        var padding = branches.Max(b => b.DirInfo.Name.Length);
         var prompt = new MultiSelectionPrompt<Branch>()
             .Title("Select branches to delete")
             .UseConverter(b => $"{b.DirInfo.Name.PadRight(padding)} -> {b.LocalName}");
