e05688a7690e1494a7e1332286fab6e2a7d98825
captainsalt <dontbothertheking@gmail.com>
2026-01-07 17:31:25 -0600

    Initial commit

---
+++ BranchManager.CLI/Program.cs
@@ -0,0 +1,26 @@
+﻿using BranchManager.Core;
+using Spectre.Console;
+using Core = BranchManager.Core;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var repositories = Core.BranchManager.GetBranches(new DirectoryInfo(""));
+        var multiSelection = GenerateMultiSelection(repositories);
+        var selected = AnsiConsole.Prompt(multiSelection);
+
+        Core.BranchManager.DeleteBranches(selected);
+    }
+
+    static MultiSelectionPrompt<Branch> GenerateMultiSelection(List<Branch> branches)
+    {
+        var padding = branches.Select(b => b.DirInfo.Name.Length).Max();
+        var prompt = new MultiSelectionPrompt<Branch>()
+            .Title("Select branches to delete")
+            .UseConverter(b => $"{b.DirInfo.Name.PadRight(padding)} -> {b.LocalName}");
+
+        prompt.AddChoices(branches);
+        return prompt;
+    }
+}
---
+++ BranchManager.Core/BranchManager.cs
@@ -0,0 +1,44 @@
+﻿using LibGit2Sharp;
+
+namespace BranchManager.Core;
+
+public record Branch(DirectoryInfo DirInfo, string LocalName, string? RemoteName);
+
+public static class BranchManager
+{
+    public static List<Branch> GetBranches(DirectoryInfo rootDir)
+    {
+        var validRepositories = rootDir.EnumerateDirectories()
+            .Where(d => Repository.IsValid(d.FullName));
+
+        var branches = new List<Branch>();
+
+        foreach (var dirInfo in validRepositories)
+        {
+            using var repo = new Repository(dirInfo.FullName);
+            var repoBranches = repo.Branches
+                .Where(b => b.FriendlyName.Trim() != "main" && b.FriendlyName.Trim() != "master")
+                .Select(b => new Branch(dirInfo, b.FriendlyName, b.RemoteName))
+                .ToList();
+
+            branches.AddRange(repoBranches);
+        }
+
+        return branches;
+    }
+
+    public static void DeleteBranches(List<Branch> branches)
+    {
+        foreach (var branch in branches)
+        {
+            using var repo = new Repository(branch.DirInfo.FullName);
+            var mainBranch = repo.Branches["master"]
+                ?? repo.Branches["main"]
+                ?? throw new Exception("Could not switch to main/master branch");
+
+            // TODO: Do this once per repository
+            Commands.Checkout(repo, mainBranch);
+            repo.Branches.Remove(branch.LocalName);
+        }
+    }
+}
