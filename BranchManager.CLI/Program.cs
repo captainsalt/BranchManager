b77d3a9d84efe45cf03b390248179264baa77d50
captainsalt <dontbothertheking@gmail.com>
2026-01-07 18:19:38 -0600

    Store root in config

--- BranchManager.CLI/Program.cs
+++ BranchManager.CLI/Program.cs
@@ -1,12 +1,13 @@
 ﻿using BranchManager.Core;
 using Spectre.Console;
 using Core = BranchManager.Core;
+using System.IO;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        var repositories = Core.BranchManager.GetBranches(new DirectoryInfo(""));
+        var repositories = Core.BranchManager.GetBranches(new DirectoryInfo(GetDirectoryPath()));
         var prompt = GeneratePrompt(repositories);
         var selected = AnsiConsole.Prompt(prompt);
 
@@ -22,5 +23,20 @@ internal class Program
 
         prompt.AddChoices(branches);
         return prompt;
+    }
+
+    static string GetDirectoryPath()
+    {
+        var configFile = "config.txt";
+        if (!File.Exists(configFile)) File.Create(configFile).Dispose();
+
+        var storedPath = File.ReadAllText(configFile).Trim();
+
+        if (Directory.Exists(storedPath))
+            return storedPath;
+
+        var desiredPath = AnsiConsole.Ask<string>("What's the root directory to look in?");
+        File.WriteAllText(configFile, desiredPath);
+        return File.ReadAllText(configFile).Trim();
     }
 }
